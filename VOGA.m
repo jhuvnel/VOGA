3908603c6dfc1d514ecd47afcd6ff49449416999
andriannaayiotis <andriannaayiotis@andriannas-mbp.lan>
2021-01-25 10:00:53 -0500

    Made AnalyzeData.m from a script into a function VOMA that uses lists for easier use.

---
+++ VOGA.m
@@ -0,0 +1,96 @@
+%% VOGA.m
+% This script should be used to segment, filter data, select cycles, and
+% parameterize batches of files. 
+% It needs to initially run from the file that it's in (so that it can add
+% all code to path).
+% Click cancel to end the loop
+% Updated on 2021-01-25
+
+opts = {'Initialize','Set Data Path','Segment','Cycle Average','Summary Table','Generate Figures','Set Version'};
+ind = 1; %Run the start procedure first
+tf1 = 1;
+while tf1
+    if strcmp(opts{ind},'Initialize')        
+        code_Path = [userpath,filesep,'VOGA']; %Assumes the scripts are in a folder called VOGA
+        addpath(genpath(code_Path))
+    elseif strcmp(opts{ind},'Set Data Path') 
+        %% Set Path
+        % Assumes you are in the right directory already
+        path = cd;
+        Raw_Path = [path,filesep,'Raw Files'];
+        Seg_Path = [path,filesep,'Segmented Files'];
+        Cyc_Path = [path,filesep,'Cycle Averages'];
+        %See if the folders already exist or need to be renamed/created
+        path_folders = extractfield(dir,'name',find(extractfield(dir,'isdir')));
+        if any(contains(path_folders,'Raw LD VOG Files'))
+            movefile('Raw LD VOG Files','Raw Files')
+        elseif ~any(contains(path_folders,'Raw Files'))
+            mkdir('Raw Files')
+        end
+        if any(contains(path_folders,'CycAvg')) 
+            movefile('CycAvg','Cycle Averages')
+        elseif any(contains(path_folders,'Cyc_Avg')) 
+            movefile('Cyc_Avg','Cycle Averages')
+        elseif ~any(contains(path_folders,'Cycle Averages'))
+            mkdir('Cycle Averages')
+        end
+        if ~any(contains(path_folders,'Segmented Files'))
+            mkdir('Segmented Files')
+        end
+    elseif strcmp(opts{ind},'Segment') 
+        %% Segment
+        path = cd;
+        Raw_Path = [path,filesep,'Raw Files'];
+        Seg_Path = [path,filesep,'Segmented Files'];
+        Cyc_Path = [path,filesep,'Cycle Averages'];
+        % Prepare to Segment
+        %Transfer NKI Raw Files from their subfolders if they exist
+        moveNKIfiles(Raw_Path)
+        %Detect and process log files and austoscan files
+        logtoNotes(Raw_Path)
+        % Select files to segment
+        all_files = extractfield(dir(Raw_Path),'name',find(~extractfield(dir(Raw_Path),'isdir')));
+        VOG_files = all_files(((contains(all_files,'SESSION')&contains(all_files,'.txt'))|contains(all_files,'.dat'))...
+            &~contains(all_files,'-Notes.txt'));
+        if isempty(VOG_files) 
+            uiwait(msgbox('No VOG files found in the Raw Files folder.'))
+        else
+            [indx,tf] = nmlistdlg('PromptString','Select files to segment:','ListSize',[300 300],'ListString',VOG_files);
+            if tf == 1
+                sel_files = VOG_files(indx);
+                for i = 1:length(sel_files)
+                    In_Path = [Raw_Path,filesep,sel_files{i}];
+                    Segment(In_Path,Seg_Path)
+                end
+            end
+        end
+    elseif strcmp(opts{ind},'Cycle Average')
+        %% Filter and select cycles
+        path = cd;
+        Raw_Path = [path,filesep,'Raw Files'];
+        Seg_Path = [path,filesep,'Segmented Files'];
+        Cyc_Path = [path,filesep,'Cycle Averages'];
+        % Get version and experimenter info from the file
+        try 
+            data = readtable([code_Path,filesep,'VerInfo.txt'],'ReadVariableNames',false);
+        catch
+            writeInfoFile(code_Path);
+            data = readtable([code_Path,filesep,'VerInfo.txt'],'ReadVariableNames',false);
+        end
+        version = data{1,2}{:};
+        Experimenter = data{2,2}{:};
+        done = false;
+        while(~done) %run until the user hits cancel on analyzing a file
+            done = MakeCycAvg(path,Seg_Path,Cyc_Path,Experimenter,version);
+        end
+    elseif strcmp(opts{ind},'Set Version')
+        writeInfoFile(code_Path);
+    end
+    %% Poll for new reponse
+    [ind,tf1] = nmlistdlg('PromptString','Select an action:',...
+                       'SelectionMode','single',...
+                       'ListSize',[100 100],...
+                       'ListString',opts,...
+                       'Position',[0,7.75,2,2.75]);    
+end
+disp('VOGA instance ended.')
--- writeInfoFile.m
+++ writeInfoFile.m
@@ -1,23 +1,16 @@
-function writeInfoFile
+function writeInfoFile(code_Path)
     prompt = {['Set VOGA parameters.',newline,newline,'Version: '];...
-    'Experimenter: ';'Code Path: '};
-    try fid = fopen('VerInfo.txt');
-        %Now get the data
-        data = cell(3,2); %a little wider than needed for formatting
-        tline = fgetl(fid);
-        k = 0;
-        while ischar(tline)
-            k = k+1;
-            data(k,1:length(split(tline,', '))) = split(tline,', ');
-            tline = fgetl(fid);
-        end
-        fclose(fid);
-        items = inputdlg(prompt,'Set VOGA Parameters',1,data(:,2));
+    'Experimenter: '};
+    try 
+        data = readtable([code_Path,filesep,'VerInfo.txt'],'ReadVariableNames',false);
+        items = inputdlg(prompt,'Set VOGA Parameters',1,data{:,2});
     catch
         items = inputdlg(prompt,'Set VOGA Parameters',1);
     end
-    info = strcat({'Version';'Experimenter';'Path'},repmat({', '},3,1),items);    
-    filePh = fopen('VerInfo.txt','w');
-    fprintf(filePh,'%s\n',info{:});
-    fclose(filePh); 
+    if ~isempty(items)
+        info = strcat({'Version';'Experimenter'},repmat({' '},2,1),items);    
+        filePh = fopen([code_Path,filesep,'VerInfo.txt'],'w');
+        fprintf(filePh,'%s\n',info{:});
+        fclose(filePh); 
+    end
 end
