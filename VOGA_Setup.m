f07885b7cc0189659723a31998044fd770f0fb5d
andriannaayiotis <andriannaayiotis@localhost>
2022-06-24 11:13:45 -0400

    v4.9.0: Previous commit has the wrong version number.

--- VOGA_Setup.m
+++ VOGA_Setup.m
@@ -3,6 +3,6 @@
 addVOGA
 copyfile('addVOGA.m',userpath)
 % Set the new version or make the VOGA_VerInfo.txt if it doesn't exist
-current_ver = 'v4.9.3';
+current_ver = 'v4.9.0';
 VOGA__setVersion(current_ver);
 VOGA__saveLastUsedParams;
