6367943598c9c6a0c38dd32992d7a360e7d47b73
ayiotis <andriannaayiotis@gmail.com>
2022-08-23 13:36:43 -0400

    Testing commits with the transferred ownership.

--- VOGA_Setup.m
+++ VOGA_Setup.m
@@ -1,6 +1,6 @@
 %% Run this script every time you pull a new version of VOGA
 % Moves a copy of addVOGA.m to the userpath
-addVOGA; %Test comment part 2
+addVOGA;
 copyfile('addVOGA.m',userpath)
 % Set the new version or make the VOGA_VerInfo.txt if it doesn't exist
 current_ver = 'v4.9.4';
